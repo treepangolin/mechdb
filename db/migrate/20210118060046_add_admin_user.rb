f4a763b40790843413a2b96872a38b8facf0a354
Jacob Koenen <1koenenjacob@gmail.com>
2021-01-30 18:38:14 -0500

    Remove irrelevant migration

--- db/migrate/20210118060046_add_admin_user.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminUser < ActiveRecord::Migration[6.1]
-  def change
-    User.create! do |u|
-      u.email = Rails.application.credentials.admin[:email]
-      u.password = Rails.application.credentials.admin[:password]
-      u.is_admin = true
-    end
-  end
-end
