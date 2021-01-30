d3c8f25972fb40d52803a49889dd5dc7f3670c3a
Jacob Koenen <1koenenjacob@gmail.com>
2021-01-30 17:43:57 -0500

    Fix postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_18_060046) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groupbuys", force: :cascade do |t|
     t.string "name"
     t.integer "category"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_060046) do
     t.string "name"
     t.string "region"
     t.text "link"
-    t.integer "groupbuy_id"
+    t.bigint "groupbuy_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["groupbuy_id"], name: "index_proxy_links_on_groupbuy_id"
