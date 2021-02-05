18179d5a0a100875918b10e88432ffe1655c4c6c
Jacob Koenen <1koenenjacob@gmail.com>
2021-02-05 14:26:16 -0500

    Too bloated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :groupbuys
-  resources :keyswitches
 
   devise_for :users, path: 'auth'
 end
--- test/controllers/keyswitches_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class KeyswitchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
