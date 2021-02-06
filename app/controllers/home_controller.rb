14ace96a7db407e145873313747948a97d6195bc
Jacob Koenen <1koenenjacob@gmail.com>
2021-02-05 20:15:56 -0500

    Barebones carousel for featured groupbuys

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @featured_groupbuys = Groupbuy.order('end_date').first(3)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,19 +10,25 @@ import "channels"
 import "stylesheets/application"
 import "@popperjs/core"
 import "bootstrap-icons/font/bootstrap-icons.css"
-import { Toast } from "bootstrap"
+import { Toast, Carousel } from "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 document.addEventListener("turbolinks:load", () => {
+  var carousel = new Carousel(document.querySelector('#featuredGroupbuys'), {
+    interval: 5000,
+    wrap: true
+  })
+
   var toastElList = [].slice.call(document.querySelectorAll('.toast'))
   var toastList = toastElList.map(function (toastEl) {
     return new Toast(toastEl)
   })
 
-  toastList.forEach(toast => toast.show());
+  carousel.cycle()
+  toastList.forEach(toast => toast.show())
 })
 
 import "controllers"
